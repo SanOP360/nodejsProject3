adb705a418571a9d7bc8c8668c67b2862ff0b10a
SanOP360 <sanjayl2001alm@gmail.com>
2024-03-04 12:48:56 +0530

    added to cart property

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart=require('../models/cart')
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -37,6 +38,14 @@ exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
     pageTitle: 'Your Cart'
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productID;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(product.id, product.price); 
+    res.redirect("/cart");
   });
 };
 
